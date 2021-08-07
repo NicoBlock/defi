8387ed8735b20c3898b645d01c2f15df579564da
nckmcfls <nckmcfls@protonmail.com>
2021-08-07 17:07:51 +0200

    Updates

---
+++ precache-manifest.55a0ab055e23c4c145196f04a1439a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a615debee4acb6d3cff54c148ef2f35a",
+    "url": "/defi/index.html"
+  },
+  {
+    "revision": "cb0cf50ef87006646eea",
+    "url": "/defi/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "e776cb6e35e35818a6a9",
+    "url": "/defi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cb0cf50ef87006646eea",
+    "url": "/defi/static/js/2.c8914219.chunk.js"
+  },
+  {
+    "revision": "e776cb6e35e35818a6a9",
+    "url": "/defi/static/js/main.d132cd3a.chunk.js"
+  },
+  {
+    "revision": "0b17224b27a3f05f2430",
+    "url": "/defi/static/js/runtime-main.30ae101b.js"
+  }
+]);
